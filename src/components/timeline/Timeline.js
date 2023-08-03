f17a3fa5c316cdfdd5135c084ad112a4487ffa98
masu <yasumoto.7991mm@gmail.com>
2023-08-03 12:00:54 +0800

    update Timeline.css

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Timeline.css";
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline--header">
+        <h2>ホーム</h2>
+      </div>
+      {/* TweetBox */}
+
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Timeline;
