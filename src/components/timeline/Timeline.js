b591a3dca7dc943779a6ee97a0605926d56ba5d7
masu <yasumoto.7991mm@gmail.com>
2023-08-05 13:52:03 +0800

    create method to extract from database

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
+import db from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function Timeline() {
+  const postData = collection(db, "posts");
+  getDocs(postData).then((querySnapshot) => {
+    console.log(querySnapshot.docs.map((doc) => doc.data()));
+  });
+
   return (
     <div className="timeline">
       {/* Header */}
